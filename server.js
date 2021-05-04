0c8f9e853263635ed83111c73309ffa49861e25c
Sawyer Cherry <sawyeracherry@gmail.com>
2021-05-03 21:41:45 -0500

    Users can sign up

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+require('dotenv').config();
 const express = require('express')
-const app = express()
 const port = 3000
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const app = express()
+
+app.use(cookieParser());
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
 
 
 // Use Body Parser
@@ -19,6 +26,7 @@ app.use(expressValidator());
 
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 // Set db
 require('./data/reddit-db');
 
@@ -36,11 +44,6 @@ app.get('/posts/new', (req, res) => {
 app.listen(port, () => {
   console.log(`We Gucci at http://localhost:${port}`)
 })
-
-
-
-
-
 
 
 module.exports = app;
